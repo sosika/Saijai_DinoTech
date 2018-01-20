cde2ce11093613d69a33dbe8b10935cc7fdc427b
sosika <hewjang@gmail.com>
2018-01-20 10:23:58 -0700

    0120

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,20 +4,59 @@
 
 // parameters: service_id, template_id, template_parameters
 
-//emailjs.send("gmail","template_t2wQnU95",{name: "DinoTech", notes: "Check this out!"})
-$('#btn').on('click', function(e){
-  e.preventDefault();
-  console.log('name', $('#name').val());
-  // if ($('#name').val() !== '' && $('#message').val() !== '') {
-    emailjs.send("gmail","blank",{Subject: $('#emailSubject').val(), To_email: $('#emailSubject').val(), notes: $('#emailBody').val()})
-    .then(function(response) {
-      console.log('response', response);
-       console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
-    }, function(err) {
-       console.log("FAILED. error=", err);
+var myform = $("form#editor-container");
+myform.submit(function(event){
+  event.preventDefault();
+
+  var params = myform.serializeArray().reduce(function(obj, item) {
+     obj[item.name] = item.value;
+     return obj;
+  }, {});
+
+  // Change to your service ID, or keep using the default service
+  var service_id = "gmail";
+
+  var template_id = "blank";
+  myform.find("button").text("Sending...");
+  emailjs.send(service_id,template_id,params)
+    .then(function(){ 
+       alert("Sent!");
+       myform.find("button").text("Send");
+     }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+       myform.find("button").text("Send");
     });
-  // } else {
-  //   console.log('one of the fields is not filled out!');
-  //   // show a popup or error modal that tells the user to fill out the form properly.
-  // }
+  return false;
+});
+
+//-------------------------------------------------------------
+//  Quill editor
+//-------------------------------------------------------------
+
+var toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
+  ['clean']                                         // remove formatting button
+];
+
+var quill = new Quill('#editor-container', {
+  modules: {
+    toolbar: toolbarOptions
+  },
+  placeholder: 'Compose an email here...',
+  theme: 'snow'  // or 'bubble'
 });
